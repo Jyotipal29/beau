e61f3a69fb0dcfb1f7eab850a61a7cafcafccc48
jyoti pal <paljyoti0129@gmail.com>
2023-07-03 22:43:27 +0530

    created cart model

---
+++ backend/model/cart.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const cartSchema = new Schema({
+  quantity: { type: Number, required: true },
+  product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  size: { type: Schema.Types.Mixed },
+  color: { type: Schema.Types.Mixed },
+});
+
+module.exports = mongoose.model("Cart", cartSchema);
