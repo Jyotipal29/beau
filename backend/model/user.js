e16a66cbd977935dc696535d26ddaafcf0852c05
jyoti pal <paljyoti0129@gmail.com>
2023-07-03 22:11:40 +0530

    updated user model

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,31 +1,12 @@
 const mongoose = require("mongoose");
-
+const { Schema } = mongoose;
 const UserSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, "Please provide a username"],
-
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Please provide a email"],
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please provide a valid email",
-      ],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-    },
-    image: {
-      type: String,
-      default:
-        "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
-    },
+    email: { type: String, required: true, unique: true },
+    password: { type: Buffer, required: true },
+    role: { type: String, required: true, default: "user" },
+    addresses: { type: [Schema.Types.Mixed] },
+    name: { type: String },
   },
 
   { timestamps: true }
