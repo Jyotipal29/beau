69bc18599a1f1435883db92115bc11579818c147
jyoti pal <paljyoti0129@gmail.com>
2023-07-04 17:00:12 +0530

    user login register fixed

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -6,10 +6,39 @@ const User = require("../model/user");
 // register
 
 const register = async (req, res) => {
-  const user = new User(req.body);
   try {
-    const doc = await user.save();
-    res.status(201).json(doc);
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "please add all fields" });
+    }
+
+    //check if user exist
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exixt" });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        addresses: user.addresses,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "invalid user" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -17,20 +46,20 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email }).exec();
-    if (!user) {
-      res.status(401).json({ message: "no such user email" });
-    } else if (user.password === req.body.password) {
-      res
-        .status(200)
-        .json({
-          _id: user._id,
-          email: user.email,
-          name: user.name,
-          addresses: user.addresses,
-        });
+    const { email, password } = req.body;
+
+    //check for user email
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        addresses: user.addresses,
+        token: generateToken(user._id),
+      });
     } else {
-      res.status(401).json({ message: "invalid credentials" });
+      res.status(400).json({ message: "Unauthorized access" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/data/product.js
+++ backend/data/product.js
@@ -1,8 +1,8 @@
 const products = [
   {
-    title: "Elegant Floral Print Maxi Dress",
+    title: "Rose Printed Co-Ord Set",
     description:
-      "Step out in style with this elegant floral print maxi dress. The vibrant floral pattern adds a touch of femininity, while the flowing silhouette creates a graceful and sophisticated look. Made with high-quality fabric, this dress offers both comfort and style. Perfect for parties, weddings, or any special occasion. Available in multiple sizes and colors to suit your preference.",
+      "Yellow and pink rose print with off white primary background colour",
     mainImageUrl:
       "https://drive.google.com/uc?id=10v2BkF4MlR6LEiX1NLUOEzlj4---VvOC",
     extraImages: [
@@ -12,15 +12,17 @@ const products = [
       "https://drive.google.com/uc?id=1BVuH0qSWCDvs_S2vb7ovraCnwUnwc7XR",
       "https://drive.google.com/uc?id=1zgeM7oJdIqa4u6VwSyCugzPu1nwcohI3",
     ],
-    price: 59.99,
-    size: ["S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 10,
+    price: 1499,
+    size: ["S", "M", "L", "XL"],
+    qty: 1,
+    InStock: 5,
+    Color: "yellow",
+    Material: "Double georgette",
   },
   {
-    title: "Vintage Lace A-Line Dress",
+    title: "Multicolour Two Tier dress ",
     description:
-      "Capture the essence of vintage charm with this lace A-line dress. The intricate lace detailing and flattering A-line silhouette create a timeless and romantic look. Made with delicate lace and lined for comfort, this dress is perfect for formal events or a night out. Pair it with your favorite accessories to complete the elegant ensemble.",
+      "Multicolour Multilayered Two Tier dress with Floral print with a colour combination of blue, green and off-white",
     mainImageUrl:
       "https://drive.google.com/uc?id=1Nh2CXLAfJpPxY9uC5V9I4wKHJRropZlq",
     extraImages: [
@@ -30,15 +32,16 @@ const products = [
       "https://drive.google.com/uc?id=1ApY_cU9ez3s63UOn61ydVd-LLKCENI5p",
       "https://drive.google.com/uc?id=1Ldqevm5w1cSwxaEwxp-VPDWfuIfc-SAE",
     ],
-    price: 79.99,
-    size: ["XS", "S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 5,
+    price: 1399,
+    size: ["S", "M", "L", "XL"],
+    qty: 1,
+    InStock: 5,
+    Color: "Multicolour",
+    Material: "Georgette",
   },
   {
-    title: "Chic Wrap Style Midi Dress",
-    description:
-      "Embrace effortless style with this chic wrap-style midi dress. The flattering wrap design enhances your silhouette, while the midi length adds a touch of sophistication. Made with a lightweight and breathable fabric, this dress is perfect for both casual and dressy occasions. Pair it with heels for a polished look or dress it down with sandals for a more relaxed vibe.",
+    title: "Maroon dress with side keyholes ",
+    description: " Maroon midi dress with side keyholes and a back zipper",
     mainImageUrl:
       "https://drive.google.com/uc?id=1THQrbjlwI8CBOeqGDgvV9MhAewagIAl5",
     extraImages: [
@@ -48,16 +51,17 @@ const products = [
       "https://drive.google.com/uc?id=1sDrCTzNiwcSAazgvIJx6wkQBiJu_ls8X",
       "https://drive.google.com/uc?id=12ScDNBghK5ng3aamjBBe31M5KuRZqYms",
     ],
-    price: 49.99,
+    price: 1250,
     size: ["S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 15,
+    qty: 1,
+    InStock: 5,
+    Color: "Maroon",
+    Material: "polycrepe",
   },
-
   {
-    title: "Bohemian Floral Print Maxi Dress",
+    title: "Floral printed black dress ",
     description:
-      "Channel your inner bohemian goddess with this stunning floral print maxi dress. The vibrant colors and flowing silhouette create a whimsical and free-spirited look. Crafted with soft and breathable fabric, this dress offers both style and comfort. Perfect for beach vacations, festivals, or any casual outing. Embrace your boho-chic style with this beautiful dress.",
+      "Black Floral printed frilled midi dress with a layer of black lining inside",
     mainImageUrl:
       "https://drive.google.com/uc?id=1TcQyUIyu8ERWYiklwsMaOmfPec4bad60",
     extraImages: [
@@ -67,15 +71,17 @@ const products = [
       "https://drive.google.com/uc?id=17Xnf-qD9aQY3Ni-_Epgfwn13Z7SHIRR_",
       "https://drive.google.com/uc?id=1EcY9linr_uW-4IGnUset9wED3nDjtfFX",
     ],
-    price: 69.99,
+    price: 1199,
     size: ["S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 8,
+    qty: 1,
+    InStock: 5,
+    Color: "black",
+    Material: "Georgette",
   },
   {
-    title: "Classic Little Black Dress",
+    title: "Brown full sleeve dress with palm leaf print ",
     description:
-      "Every woman needs a classic little black dress in her wardrobe, and this one is a timeless choice. With its sleek silhouette and versatile design, this dress is perfect for any occasion. Made with high-quality fabric, it offers a comfortable and flattering fit. Dress it up with statement jewelry and heels for a sophisticated evening look or pair it with flats for a chic daytime ensemble.",
+      "Brown full sleeve midi dress with palm leaf print and a black lining inside the dress",
     mainImageUrl:
       "https://drive.google.com/uc?id=1Xu3ncJMHy85BArU-vH_Lx9WrpWjybqEP",
     extraImages: [
@@ -85,15 +91,17 @@ const products = [
       "https://drive.google.com/uc?id=1N0P1kuw6RhgEh97l2O6pExE3oY7KV3mM",
       "https://drive.google.com/uc?id=1qTyCVZghU7ImC0DIYJbfJXSJpjDQYzWS",
     ],
-    price: 89.99,
-    size: ["XS", "S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 12,
+    price: 1250,
+    size: ["S", "M", "L", "XL"],
+    qty: 1,
+    InStock: 5,
+    Color: "Brown",
+    Material: "Chiffon",
   },
   {
-    title: "Flirty Ruffled Wrap Dress",
+    title: "Yellow Rose printed front slit dress ",
     description:
-      "Make a statement with this flirty ruffled wrap dress. The feminine ruffles and wrap design add a touch of elegance and playfulness. Crafted with lightweight and breathable fabric, this dress ensures comfort all day long. Whether it's a date night or a girls' night out, this dress is sure to turn heads. Pair it with heels and minimal accessories to let the dress shine.",
+      "Front slit dress with lapel collar. Yellow and pink rose Printed dress with an off-white background",
     mainImageUrl:
       "https://drive.google.com/uc?id=1A19sR2sfHceAmCLtaTLTCCqbz8rB--I_",
     extraImages: [
@@ -103,16 +111,17 @@ const products = [
       "https://drive.google.com/uc?id=18YkVHASdkihlieLCOtMNFmTcOIB5RTLg",
       "https://drive.google.com/uc?id=11yXsUx3_VlsqlKWCQLhxMGMcuugS70TD",
     ],
-    price: 54.99,
-    size: ["S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 10,
+    price: 999,
+    size: ["S", "M", "L", "XL"],
+    qty: 1,
+    InStock: 5,
+    Color: "Yellow",
+    Material: "Heavy Georgette",
   },
-
   {
-    title: "Sophisticated Lace Overlay Dress",
+    title: "Wine Colour Off shoulder dress",
     description:
-      "Exude elegance and sophistication with this lace overlay dress. The intricate lace detailing adds a touch of femininity, while the classic silhouette creates a timeless look. Made with high-quality fabric, this dress offers both comfort and style. Perfect for formal events, weddings, or any special occasion. Elevate your wardrobe with this stunning dress.",
+      "Wine Colour Off shoulder A-line dress Cross Front and Back design with a back zipper.",
     mainImageUrl:
       "https://drive.google.com/uc?id=1JUOTVMrRupu0madWrsp5QzS1oOkctIwl",
     extraImages: [
@@ -122,15 +131,16 @@ const products = [
       "https://drive.google.com/uc?id=1o1T7A8qNP8jdL5_6lo9zB7dTr26HZfm5",
       "https://drive.google.com/uc?id=1V9QcCokrTIPu2RMDddk1D-Q10BkxJJgm",
     ],
-    price: 79.99,
+    price: 1199,
     size: ["S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 10,
+    qty: 1,
+    InStock: 5,
+    Color: "Wine",
+    Material: " polycrepe",
   },
   {
-    title: "Effortless Boho Maxi Dress",
-    description:
-      "Capture the essence of bohemian style with this effortless boho maxi dress. The flowy and relaxed fit makes it perfect for laid-back occasions. Made with lightweight and breathable fabric, this dress is both comfortable and stylish. Whether it's a beach getaway or a casual day out, this dress will keep you looking effortlessly chic.",
+    title: "Front plete full sleeve maroon top",
+    description: "Front plete full sleeve maroon top with a middle front slit",
     mainImageUrl:
       "https://drive.google.com/uc?id=10qZRJuDzf_DpzGAvF-76S2LZKn-4aqYE",
     extraImages: [
@@ -140,15 +150,16 @@ const products = [
       "https://drive.google.com/uc?id=18YM8VyADt5Puu2T0jnEA2aGstD_P3wvr",
       "https://drive.google.com/uc?id=1mRzUhpNfJ6YdRcw_FFSsJ6epjoWogmeN",
     ],
-    price: 49.99,
-    size: ["XS", "S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 15,
+    price: 699,
+    size: ["S", "M", "L", "XL"],
+    qty: 1,
+    InStock: 5,
+    Color: " maroon",
+    Material: " polycrepe",
   },
   {
-    title: "Charming Polka Dot Swing Dress",
-    description:
-      "Channel retro vibes with this charming polka dot swing dress. The playful polka dot print and swing silhouette create a fun and flirty look. Made with soft and stretchy fabric, this dress offers all-day comfort. Perfect for parties, picnics, or any daytime event. Embrace your feminine side with this delightful dress.",
+    title: "One shoulder frilled top",
+    description: "One Shoulder dress with a round frill at the top",
     mainImageUrl:
       "https://drive.google.com/uc?id=1JRgumVV78BPDIb_yeh1x-q7qws_ExZiP",
     extraImages: [
@@ -158,15 +169,16 @@ const products = [
       "https://drive.google.com/uc?id=1VDpK21q-9F_BWtzgkkpFtjXImNxRemaq",
       "https://drive.google.com/uc?id=1EFbjUmDUui4xlGn_S5Eq55CoOVFvemx3",
     ],
-    price: 39.99,
+    price: 599,
     size: ["S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 20,
+    qty: 1,
+    InStock: 5,
+    Color: "Bottle Green",
+    Material: " polycrepe",
   },
   {
-    title: "Romantic Off-Shoulder Lace Dress",
-    description:
-      "Indulge in romance with this stunning off-shoulder lace dress. The delicate lace detailing and off-shoulder neckline create a dreamy and feminine look. Made with a soft and stretchy fabric, this dress ensures a comfortable fit. Perfect for date nights, weddings, or any special occasion where you want to make a lasting impression.",
+    title: "One shoulder frilled top",
+    description: "One Shoulder dress with a round frill at the top",
     mainImageUrl:
       "https://drive.google.com/uc?id=1xWMJND128wJ787aIYF6CawmYoi5aEWZU",
     extraImages: [
@@ -176,119 +188,11 @@ const products = [
       "https://drive.google.com/uc?id=1tt56dTjSdrJZhmrqNLvl0R0HrJjouSpF",
       "https://drive.google.com/uc?id=1Ejdzf5cY7iBinI5ixalbzZrLAB60-nTn",
     ],
-    price: 89.99,
-    size: ["XS", "S", "M"],
-    category: "Women's Dresses",
-    qty: 8,
-  },
-  {
-    title: "Playful Striped Wrap Dress",
-    description:
-      "Add a playful touch to your wardrobe with this striped wrap dress. The vibrant stripes and flattering wrap design create a stylish and eye-catching look. Made with lightweight and breathable fabric, this dress is perfect for warmer days. Dress it up with heels or go for a casual vibe with sandals. Embrace the joy of fashion with this fun and trendy dress.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=1ma0KAPe81v6QTmVRbua3FPHt3eDIzp_G",
-    extraImages: [
-      "https://drive.google.com/uc?id=1bPo0w39_-yZYGJ2dzM6jyX2xBCgCalVL",
-      "https://drive.google.com/uc?id=16SKvpRYtveIYXqN1cap7EYJH-RC5RkKY",
-      "https://drive.google.com/uc?id=1O3hXklnfbKm6SSqCykwDL1xWOURGxWEA",
-      "https://drive.google.com/uc?id=1pHjvmkRI1JIB9eNcMsaNU7K9nStG5CEg",
-      "https://drive.google.com/uc?id=1iKZPKgyXHbIqptSqC4e8V7rpU5rc7a0L",
-    ],
-    price: 59.99,
+    price: 599,
     size: ["S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 12,
-  },
-  {
-    title: "Sleek Sleeveless Bodycon Dress",
-    description:
-      "Make a statement with this sleek sleeveless bodycon dress. The figure-hugging silhouette accentuates your curves and exudes confidence. Made with a stretchy and comfortable fabric, this dress is perfect for a night out or any special event. Pair it with heels and statement accessories for a glamorous look that will turn heads.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=1Cn5Ney-UPJ9Mfoa47--p_NymefBb_vbv",
-    extraImages: [
-      "https://drive.google.com/uc?id=1kVSDrRSsYI-YBCu-yN6K9yqtKYPN7Sjh",
-      "https://drive.google.com/uc?id=1GpiL4QPXlPeMxPvgrSBhnVF5wZgN3SsJ",
-      "https://drive.google.com/uc?id=1cb2eXQjygMpUty-0R43cMxlTEhNpwtn_",
-      "https://drive.google.com/uc?id=1lOyPBaCsysO6GR2zg1hsnOupf23E9xCp",
-      "https://drive.google.com/uc?id=1FqGt5ljaSUn-TeenRtV5c6WizR9kYWOy",
-    ],
-    price: 69.99,
-    size: ["XS", "S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 10,
-  },
-  {
-    title: "Elegant Satin Evening Gown",
-    description:
-      "Make a grand entrance with this elegant satin evening gown. The floor-length design and luxurious satin fabric create a regal and sophisticated look. The fitted bodice and flowing skirt flatter your figure, while the off-shoulder neckline adds a touch of allure. Perfect for black-tie events, galas, or formal weddings. Feel like a queen in this exquisite gown.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=1dNv0NPmXGHLPhTexFnQ3-klc69wKVjB4",
-    extraImages: [
-      "https://drive.google.com/uc?id=1OUhm-1QrHKgrAWH3D1u5cskNQMGpSIGV",
-      "https://drive.google.com/uc?id=13CoKZYRdwY1kYduI_zaOExAPhtVASL5R",
-      "https://drive.google.com/uc?id=1CzTf6J0icGP27WfYJF6Q2mV8kEZTujU4",
-      "https://drive.google.com/uc?id=19OZixL2Xjz3vNJ4kicb3cJ8pal4lOTC-",
-      "https://drive.google.com/uc?id=1Ch_ghlmXr6Ig0sW812-5mTUZTNifa4fE",
-    ],
-    price: 129.99,
-    size: ["S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 5,
-  },
-  {
-    title: "Effortless T-Shirt Dress",
-    description:
-      "Stay comfortable and stylish with this effortless t-shirt dress. The relaxed fit and soft fabric make it perfect for everyday wear. Dress it up with a statement belt and heels, or keep it casual with sneakers. Versatile and easy to style, this dress is a must-have in any wardrobe.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=1GuIBJupD3vEC_8wv_PxtMb6HpXzgYLmk",
-    extraImages: [
-      "https://drive.google.com/uc?id=1J9gC1QrwWJZrVdpBqWaTYatKr-TzDhfL",
-      "https://drive.google.com/uc?id=1tFzKfdQnqNGjg7bWFQlU9j6ESTlpMsV-",
-      "https://drive.google.com/uc?id=1ygjcJUzzPmn1qqafT0WMetPvFU7VFZHE",
-      "https://drive.google.com/uc?id=1OroJci-Mz1K3KgOO5s1IdtRLuJQo3E1Y",
-      "https://drive.google.com/uc?id=1Dnf2Z3SP9qhSd4ZjzW7fMWrifF0Kflql",
-    ],
-    price: 34.99,
-    size: ["XS", "S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 15,
-  },
-  {
-    title: "Chic Belted Shirt Dress",
-    description:
-      "Achieve a polished and sophisticated look with this chic belted shirt dress. The classic shirt collar and waist belt create a tailored and flattering silhouette. Made with a lightweight and breathable fabric, this dress is perfect for both office and casual wear. Pair it with heels for a professional look or dress it down with flats for a more relaxed vibe.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=116pBMwhn7ELz15qE3Ed059Tk7AFgbRaR",
-    extraImages: [
-      "https://drive.google.com/uc?id=1amhCvxKGhKaR3bbeLFie67ZibuThWiyN",
-      "https://drive.google.com/uc?id=1M0jzUwSsED6Ykvb8o_rm9cviNFbqlYa5",
-      "https://drive.google.com/uc?id=1l5A-fI3E_KCbwS2NN3l_DtlBkcDs1Srf",
-      "https://drive.google.com/uc?id=1FDIOJ5MujTQpd1z9wCHd8pcjipOnq_yP",
-      "https://drive.google.com/uc?id=1ytymoNREWxe7Arxj4dESWyY9bMhaoES2",
-    ],
-    price: 49.99,
-    size: ["S", "M", "L"],
-    category: "Women's Dresses",
-    qty: 20,
-  },
-  {
-    title: "Bold Floral Print Skater Dress",
-    description:
-      "Make a statement with this bold floral print skater dress. The vibrant colors and playful floral pattern create a lively and eye-catching look. Made with a stretchy and comfortable fabric, this dress offers both style and ease of movement. Whether it's a garden party or a summer outing, this dress will make you stand out in style.",
-    mainImageUrl:
-      "https://drive.google.com/uc?id=1lDKnUC4fL-iWZjBQgMDN48WeWwe_0c1J",
-    extraImages: [
-      "https://drive.google.com/uc?id=11Z7uUbarozRGcWAG-EDX2gw0fQ3yOWUc",
-      "https://drive.google.com/uc?id=1DVw_10v_PacNQsT_tYj8XiCagoaC-0d_",
-      "https://drive.google.com/uc?id=1WnCjPzW9XH7w9gKLZ95O0OVYVq5Ndawy",
-      "https://drive.google.com/uc?id=1Pn-HxQkAMzW0QNlxberCJ2uxRhNJeE6u",
-      "https://drive.google.com/uc?id=1bRrfMyhG8CbR1PugGuEaIOAHQLAGT5-l",
-    ],
-    price: 44.99,
-    size: ["XS", "S", "M", "L", "XL"],
-    category: "Women's Dresses",
-    qty: 18,
+    qty: 1,
+    InStock: 5,
+    Color: " Polka dot",
+    Material: " polycrepe",
   },
 ];
-
-module.exports = products;
