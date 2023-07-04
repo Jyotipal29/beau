617f8827cf7d9b19feb0baee9f8046863bc37d4b
jyoti pal <paljyoti0129@gmail.com>
2023-07-04 23:41:49 +0530

    navbar fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,7 +54,6 @@ const Navbar = ({ children }) => {
                             to={item.link}
                             className="text-gray-300 hover:bg-gray-700 hover:text-white
                             rounded-md px-3 py-2 text-sm font-medium"
-                            aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
                           </Link>
@@ -152,7 +151,6 @@ const Navbar = ({ children }) => {
                             : "text-gray-300 hover:bg-gray-700 hover:text-white",
                           "block rounded-md px-3 py-2 text-base font-medium"
                         )}
-                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Disclosure.Button>
