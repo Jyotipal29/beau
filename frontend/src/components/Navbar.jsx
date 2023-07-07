a6235edb454f2429ab813ca690c4c0812d07d806
jyoti pal <paljyoti0129@gmail.com>
2023-07-07 09:58:56 +0530

    navigation fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ import {
   ShoppingCartIcon,
   HeartIcon,
 } from "@heroicons/react/24/outline";
-import { Link, Navigate } from "react-router-dom";
-import logo from "../assets/beau-logo.jpg";
+import { Link } from "react-router-dom";
+import logo from "../assets/beau-new-logo.jpg";
 import { useUser } from "../context/userContext/context";
 
 const navigation = [
@@ -32,29 +32,32 @@ const Navbar = ({ children }) => {
   } = useUser();
   return (
     <>
-      {user === null && <Navigate to="/login" />}
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
+      <div className="min-h-full z-50">
+        <Disclosure
+          as="nav"
+          className={`sticky top-0 z-50  w-full px-10 !bg-red-600 `}
+        >
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                  <div className="">
+              <div className=" ">
+                <div className="flex h-16 items-center lg:ml-6 md:ml-0 rounded-lg">
+                  <Link href="/">
                     <img
-                      className="h-12 w-52 object-cover object-center"
+                      className="h-12 w-52 object-contain object-center"
                       src={logo}
                       alt="Beau"
                     />
-                  </div>
+                  </Link>
                   <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
+                    <div className=" flex items-baseline space-x-4">
                       {navigation.map((item) =>
                         item[user.role] ? (
                           <Link
                             key={item.name}
                             to={item.link}
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white
-                            rounded-md px-3 py-2 text-sm font-medium"
+                            className="text-white hover:bg-red-500 bg-red-600  font-bold
+                            rounded-md px-3 py-2 text-sm"
+                            aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
                           </Link>
@@ -62,32 +65,34 @@ const Navbar = ({ children }) => {
                       )}
                     </div>
                   </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
+                  <div className="hidden z-50 md:block md:ml-auto lg:mr-20 md:mr-8">
+                    <div className="flex items-center">
                       <button
                         type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        className="rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 "
                       >
                         <Link to="/cart">
                           <ShoppingCartIcon
-                            className="h-6 w-6"
+                            className="h-[38px] w-[38px] bg-transparent p-2 rounded-full "
+                            stroke="white"
                             aria-hidden="true"
                           />
                         </Link>
                       </button>
-                      <button
-                        type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
+                      <button type="button" className="rounded-full  ">
                         <Link to="/wish">
-                          <HeartIcon className="h-6 w-6" aria-hidden="true" />
+                          <HeartIcon
+                            className="h-[38px] w-[38px] bg-transparent p-2 rounded-full "
+                            stroke="white"
+                            aria-hidden="true"
+                          />
                         </Link>
                       </button>
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
                         <div>
-                          <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <Menu.Button className="flex max-w-xs items-center rounded-full  text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-red-600">
                             <span className="sr-only">Open user menu</span>
                             <img
                               className="h-8 w-8 rounded-full"
@@ -105,19 +110,19 @@ const Navbar = ({ children }) => {
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-red-600 overflow-hidden shadow-lg ring-1  ring-opacity-5 focus:outline-none">
                             {userNavigation.map((item) => (
                               <Menu.Item key={item.name}>
                                 {({ active }) => (
-                                  <Link
-                                    to={item.link}
+                                  <a
+                                    href={item.href}
                                     className={classNames(
                                       active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
+                                      "block px-4 py-2 text-sm text-red-600  md:bg-white md:hover:bg-gray-100"
                                     )}
                                   >
                                     {item.name}
-                                  </Link>
+                                  </a>
                                 )}
                               </Menu.Item>
                             ))}
@@ -126,9 +131,9 @@ const Navbar = ({ children }) => {
                       </Menu>
                     </div>
                   </div>
-                  <div className="-mr-2 flex md:hidden">
+                  <div className="-mr-2 ml-auto flex md:hidden">
                     {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md  p-2  hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2  mr-5">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
                         <XMarkIcon
@@ -156,17 +161,18 @@ const Navbar = ({ children }) => {
                         className={classNames(
                           item.current
                             ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "block rounded-md px-3 py-2 text-base font-medium"
+                            : "text-white",
+                          "block rounded-md px-3 py-2 text-base font-medium "
                         )}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Link>
                     ) : null
                   )}
                 </div>
-                <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
+                <div className="border-t  pb-3 pt-4">
+                  <div className="flex items-center px-5 bg-red-600">
                     <div className="flex-shrink-0">
                       <img
                         className="h-10 w-10 rounded-full"
@@ -181,7 +187,7 @@ const Navbar = ({ children }) => {
                     </div>
                     <button
                       type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      className="ml-auto flex-shrink-0 rounded-full  p-1  text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     >
                       <span className="sr-only">View notifications</span>
                       <Link to="/cart">
@@ -193,7 +199,7 @@ const Navbar = ({ children }) => {
                     </button>
                     <button
                       type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      className="ml-auto flex-shrink-0 rounded-full  p-1  text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     >
                       <span className="sr-only">View notifications</span>
                       <Link to="/wish">
@@ -203,13 +209,14 @@ const Navbar = ({ children }) => {
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
-                      <Link
+                      <Disclosure.Button
                         key={item.name}
-                        to={item.link}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium  hover:bg-red-600 text-white"
                       >
                         {item.name}
-                      </Link>
+                      </Disclosure.Button>
                     ))}
                   </div>
                 </div>
@@ -219,9 +226,7 @@ const Navbar = ({ children }) => {
         </Disclosure>
 
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {children}
-          </div>
+          <div className="mx-auto max-w-7xl">{children}</div>
         </main>
       </div>
     </>
