610b8f04ad47448a5956c8b67c621aa415e8a5e7
jyoti pal <paljyoti0129@gmail.com>
2023-07-08 13:56:51 +0530

    created hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,9 @@
 const Hero = () => {
   return (
     <div>
-      <section className="mt-1 min-w-screen flex justify-center max-md:pl-4 items-start space-y-6 flex-col text-white md:pl-20 lg:pl-40 [background-image:url('hero.jpeg')] bg-cover bg-right w-full min-h-[600px] max-md:[background-position:-550px_0] max-sm:[background-position:-650px_0] bg-no-repeat">
-        {/* <span className="max-lg:block hidden w-full h-full bg-slate-500/70 absolute"></span> */}
+      <section
+        className={`mt-1 min-w-screen flex justify-center max-md:pl-4 items-start space-y-6 flex-col text-white md:pl-20 lg:pl-40 [background-image:url("new-hero-img.jpg")] bg-cover bg-right w-full min-h-[600px] max-md:[background-position:-550px_0] max-sm:[background-position:-650px_0] bg-no-repeat`}
+      >
         <b className="font-bold">Trendy Collenction</b>
         <h1 className="text-7xl max-md:text-6xl font-extrabold">
           Perfect
