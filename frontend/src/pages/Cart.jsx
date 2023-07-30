89b8ddd333fb3516982786f8a76a43d743b15fec
jyoti pal <76580894+Jyotipal29@users.noreply.github.com>
2023-07-30 13:31:44 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallBack, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -20,7 +20,7 @@ const Cart = () => {
     userState: { user },
   } = useUser();
 
-  const getCart = async () => {
+  const getCart = useCallBack(async () => {
     try {
       setLoading(true);
       const config = {
@@ -36,13 +36,13 @@ const Cart = () => {
       console.log(error.message);
       setLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     getCart();
-  }, []);
+  }, [getCart]);
 
-  const { totalPrice, totalItems } = cart.reduce(
+  const { totalPrice } = cart.reduce(
     (accumulator, item) => {
       const { product, quantity } = item;
       const { price } = product; // Assuming `price` property exists in the `product` object
@@ -75,10 +75,6 @@ const Cart = () => {
     });
   };
 
-  useEffect(() => {
-    console.log("my cart", cart);
-  });
-
   const removeItem = async (id) => {
     try {
       setSLoading((prev) => ({ ...prev, [id]: true }));
@@ -89,8 +85,7 @@ const Cart = () => {
         },
       };
 
-      const { data } = await axios.delete(`${api}cart/${id}`, config);
-      console.log(data, "delted data");
+      await axios.delete(`${api}cart/${id}`, config);
       productDispatch({ type: "REMOVE_FROM_CART", payload: id });
       toast.success("product removed", {
         position: "top-center",
@@ -128,7 +123,7 @@ const Cart = () => {
           <Loader loading={loading} />
         ) : (
           <>
-            {cart.length === 0 && (
+            {cart.length === 0 ? (
               <div className="flex flex-col  items-center space-y-8 mt-4">
                 <h1 className="text-2xl uppercase font-lora font-bold">
                   your cart is empty
@@ -137,80 +132,80 @@ const Cart = () => {
                   <Link to="/products">shop here</Link>
                 </button>
               </div>
-            )}
+            ) : (
+              <div className="flex flex-col md:grid grid-cols-5 lg:grid-cols-3 gap-5">
+                <div className="md:col-span-3 lg:col-span-2 border">
+                  <p className="p-2 m-2">
+                    Total items in your bag: <b>{cart.length}</b>
+                  </p>
+                  {cart.map(({ _id, product, quantity, size }) => (
+                    <div key={product._id} className="flex border p-1 m-2">
+                      <img
+                        src={product.mainImageUrl}
+                        alt={product.imageAlt}
+                        width="100px"
+                        height="60px"
+                      />
+                      <div className="flex flex-col justify-arround gap-2">
+                        <h3 className="font-bold text-lg">{product.title}</h3>
 
-            <div className="flex flex-col md:grid grid-cols-5 lg:grid-cols-3 gap-5">
-              <div className="md:col-span-3 lg:col-span-2 border">
-                <p className="p-2 m-2">
-                  Total items in your bag: <b>{cart.length}</b>
-                </p>
-                {cart.map(({ _id, product, quantity, size }) => (
-                  <div key={product._id} className="flex border p-1 m-2">
-                    <img
-                      src={product.mainImageUrl}
-                      alt={product.imageAlt}
-                      width="100px"
-                      height="60px"
-                    />
-                    <div className="flex flex-col justify-arround gap-2">
-                      <h3 className="font-bold text-lg">{product.title}</h3>
+                        <div className="flex gap-5">
+                          <span>
+                            Size: <b>{size}</b>
+                          </span>
 
-                      <div className="flex gap-5">
-                        <span>
-                          Size: <b>{size}</b>
-                        </span>
+                          <span>
+                            Qty:{" "}
+                            <select
+                              value={quantity}
+                              onChange={(e) => handleQty(e, _id)}
+                              className="pl-3 py-0"
+                            >
+                              {[1, 2, 3, 4, 5].map((i) => (
+                                <option key={i} value={i}>
+                                  {i}
+                                </option>
+                              ))}
+                            </select>
+                          </span>
+                        </div>
 
-                        <span>
-                          Qty:{" "}
-                          <select
-                            value={quantity}
-                            onChange={(e) => handleQty(e, _id)}
-                            className="pl-3 py-0"
-                          >
-                            {[1, 2, 3, 4, 5].map((i) => (
-                              <option key={i} value={i}>
-                                {i}
-                              </option>
-                            ))}
-                          </select>
-                        </span>
+                        <b>MRP: {product.price}</b>
                       </div>
-
-                      <b>MRP: {product.price}</b>
+                      <button
+                        onClick={() => removeItem(_id)}
+                        className="ml-auto p-2 text-red-600 self-start hover:border-red-600 hover:border"
+                      >
+                        {sLoading[_id] ? "Removing.." : "Remove"}
+                      </button>
                     </div>
-                    <button
-                      onClick={() => removeItem(_id)}
-                      className="ml-auto p-2 text-red-600 self-start hover:border-red-600 hover:border"
+                  ))}
+                </div>
+
+                <div className="flex flex-col md:col-span-2 lg:col-span-1 gap-5 border p-4 max-md:mt-20">
+                  <b className="">Order Details</b>
+                  <p className="flex items-center justify-between pb-4">
+                    Bag Total <b>{Math.floor(totalPrice)}</b>
+                  </p>
+                  <p className="flex items-center justify-between mb-auto">
+                    Total Amount <b>RS: {totalPrice}</b>
+                  </p>
+
+                  <div className="flex justify-between items-stretch h-14 border bg-white">
+                    <div className="w-1/2 flex flex-col items-center justify-center">
+                      <b>RS: {totalPrice}</b>
+                      <span>Total Amount</span>
+                    </div>
+                    <Link
+                      to="/checkout"
+                      className="w-1/2 bg-red-600 text-white flex items-center justify-center hover:font-bold"
                     >
-                      {sLoading[_id] ? "Removing.." : "Remove"}
-                    </button>
+                      Proceed
+                    </Link>
                   </div>
-                ))}
-              </div>
-
-              <div className="flex flex-col md:col-span-2 lg:col-span-1 gap-5 border p-4 max-md:mt-20">
-                <b className="">Order Details</b>
-                <p className="flex items-center justify-between pb-4">
-                  Bag Total <b>{Math.floor(totalPrice)}</b>
-                </p>
-                <p className="flex items-center justify-between mb-auto">
-                  Total Amount <b>RS: {totalPrice}</b>
-                </p>
-
-                <div className="flex justify-between items-stretch h-14 border bg-white">
-                  <div className="w-1/2 flex flex-col items-center justify-center">
-                    <b>RS: {totalPrice}</b>
-                    <span>Total Amount</span>
-                  </div>
-                  <Link
-                    to="/checkout"
-                    className="w-1/2 bg-red-600 text-white flex items-center justify-center hover:font-bold"
-                  >
-                    Proceed
-                  </Link>
                 </div>
               </div>
-            </div>
+            )}
           </>
         )}
         <ToastContainer />
